0b4691063df99d59c10441d37679e0c853d27bb8
ThaminduChankana <it20604530@my.sliit.lk>
2022-10-29 12:18:34 +0530

    added extra curricular activities tab

---
+++ src/components/Extra.js
@@ -0,0 +1,117 @@
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import "react-multi-carousel/lib/styles.css";
+import "react-appear-on-scroll/dist/index.css";
+import star from "../assets/img/star.svg";
+import { AppearingContainer } from "react-appear-on-scroll";
+import "react-appear-on-scroll/dist/index.css";
+import { Row } from "react-bootstrap";
+
+export const Extra = () => {
+	return (
+		<section className="extra" id="extras">
+			<div className="container">
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<Row>
+					<div className=" extra-bx wow zoomIn" style={{ height: "auto" }}>
+						<h2>Extra Curricular Activities</h2>
+						<br></br>
+                        <br></br>
+
+						<div>
+							<center>
+								<AppearingContainer>
+									<div>
+										<h5 style={{ width: "100%", padding: "0 10px 0 10px" }}>
+											<svg
+												width="1em"
+												height="1em"
+												viewBox="0 0 16 16"
+												className="bi bi-trophy-fill"
+												fill="#FFD700"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													fillRule="evenodd"
+													d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"
+												></path>
+											</svg>
+											&nbsp;Committee Member SLIIT Faculty Of Computing<br></br>Students' Community
+										</h5>
+									</div>
+								</AppearingContainer>
+                                <br></br>
+								<AppearingContainer>
+									<div>
+										<h5 style={{ width: "100%", padding: "0 10px 0 10px" }}>
+											<svg
+												width="1em"
+												height="1em"
+												viewBox="0 0 16 16"
+												className="bi bi-trophy-fill"
+												fill="#FFD700"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													fillRule="evenodd"
+													d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"
+												></path>
+											</svg>
+											&nbsp;Member MS Club of SLIIT
+										</h5>
+									</div>
+								</AppearingContainer>
+                                <br></br>
+								<AppearingContainer>
+									<div>
+										<h5 style={{ width: "100%", padding: "0 10px 0 10px" }}>
+											<svg
+												width="1em"
+												height="1em"
+												viewBox="0 0 16 16"
+												className="bi bi-trophy-fill"
+												fill="#FFD700"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													fillRule="evenodd"
+													d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"
+												></path>
+											</svg>
+											&nbsp;Member SLIIT FOSS Community
+										</h5>
+									</div>
+								</AppearingContainer>
+							</center>
+						</div>
+						<br></br>
+						<br></br>
+						<center>
+							<img src={star} alt="star" style={{ width: "2%" }}></img>
+							<img src={star} alt="star" style={{ width: "2%" }}></img>
+							<img src={star} alt="star" style={{ width: "2%" }}></img>
+						</center>
+						<br></br>
+
+						<h3 style={{ padding: 10 }}>
+							Please visit my LinkedIn profile or view my resume to get the updated details of my Extra Curricular
+							Activities...
+						</h3>
+						<a href="https://www.linkedin.com/in/thamindu-gamage-5391b5192/">
+							<br></br>
+							<center>
+								<div style={{ width: "4%" }}>
+									<img src={navIcon1} alt="linkedin" />
+								</div>
+							</center>
+						</a>
+					</div>
+				</Row>
+			</div>
+			<br></br>
+			<br></br>
+		</section>
+	);
+};
